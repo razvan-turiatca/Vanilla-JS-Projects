9dab73fd448c8cc1d0fbd577c4f29e060c3b21d5
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-02 01:33:44 +0200

    created functionality for moving up and movinf down

--- Catch-Me-If-You-Can-Game/game.js
+++ Catch-Me-If-You-Can-Game/game.js
@@ -19,14 +19,40 @@ const renderMap = (map) => {
 
 renderMap(map)
 
+// movement
 const moveRight = (map) => {
   let idx = map.indexOf('p')
-  return map.slice(0, idx).concat('.p', map.slice(idx + 2))
+  return map[idx + 1] != '#'
+    ? map.slice(0, idx).concat('.p', map.slice(idx + 2))
+    : map
 }
 
 const moveLeft = (map) => {
   let idx = map.indexOf('p')
-  return map.slice(0, idx - 1).concat('p.', map.slice(idx + 1))
+  return map[idx - 1] != '#'
+    ? map.slice(0, idx - 1).concat('p.', map.slice(idx + 1))
+    : map
+}
+
+const moveDown = (map) => {
+  let idx = map.indexOf('p')
+  console.log(map.slice(0, idx))
+  return map[idx + 12] != '#'
+    ? map
+        .slice(0, idx)
+        .concat('.', map.slice(idx + 1, idx + 11))
+        .concat('p', map.slice(idx + 12))
+    : map
+}
+
+const moveUp = (map) => {
+  let idx = map.indexOf('p')
+  return map[idx - 11] != '#'
+    ? map
+        .slice(0, idx - 11)
+        .concat('p', map.slice(idx - 10, idx))
+        .concat('.', map.slice(idx + 1))
+    : map
 }
 
 renderMap(map)
