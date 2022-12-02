6e0dd87332cb0eac67fd1f776685aef922be8fc5
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-02 20:45:38 +0200

    refactored movement code and map design

--- Catch-Me-If-You-Can-Game/game.js
+++ Catch-Me-If-You-Can-Game/game.js
@@ -1,62 +1,73 @@
 let map = `
-□□□□□□□□□□
-□▫▫●▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□▫▫▫▫▫▫▫▫□
-□□□□□□□□□□
+▧▧▧▧▧▧▧▧▧▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◉◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧◻◻◻◻◻◻◻◻▧
+▧▧▧▧▧▧▧▧▧▧
 `
+
+const hero = '◉'
+const enemy = '〰'
+const path = '◻'
+const wall = '▧'
 
 const renderMap = (map) => {
   gameMapContainer.innerHTML = map
 }
 
-renderMap(map)
-
 // movement
 const moveRight = (map) => {
-  let idx = map.indexOf('●')
-  return map[idx + 1] != '□'
-    ? map.slice(0, idx).concat('▫●', map.slice(idx + 2))
+  let idx = map.indexOf(hero)
+  return map[idx + 1] != wall
+    ? map.slice(0, idx).concat(path, hero, map.slice(idx + 2))
     : map
 }
 
 const moveLeft = (map) => {
-  let idx = map.indexOf('●')
-  return map[idx - 1] != '□'
-    ? map.slice(0, idx - 1).concat('●▫', map.slice(idx + 1))
+  let idx = map.indexOf(hero)
+  return map[idx - 1] != wall
+    ? map.slice(0, idx - 1).concat(hero, path, map.slice(idx + 1))
     : map
 }
 
 const moveDown = (map) => {
-  let idx = map.indexOf('●')
+  let idx = map.indexOf(hero)
   // console.log(map.slice(0, idx))
-  return map[idx + 12] != '□'
+  return map[idx + 12] != wall
     ? map
         .slice(0, idx)
-        .concat('▫', map.slice(idx + 1, idx + 11))
-        .concat('●', map.slice(idx + 12))
+        .concat(path, map.slice(idx + 1, idx + 11))
+        .concat(hero, map.slice(idx + 12))
     : map
 }
 
 const moveUp = (map) => {
-  let idx = map.indexOf('●')
-  return map[idx - 11] != '□'
+  let idx = map.indexOf(hero)
+  return map[idx - 11] != wall
     ? map
         .slice(0, idx - 11)
-        .concat('●', map.slice(idx - 10, idx))
-        .concat('▫', map.slice(idx + 1))
+        .concat(hero, map.slice(idx - 10, idx))
+        .concat(path, map.slice(idx + 1))
     : map
 }
 
+const idx = (el) => {
+  return map.indexOf(el)
+}
+
+const shoot = (map) => {
+  // let idx = map.indexOf(hero)
+  return map.slice(0, idx(hero) - 1).concat('◅', map.slice(idx(hero)))
+}
+
 function action(e) {
-  console.log(e)
   if (e.code == 'ArrowDown') {
     map = moveDown(map)
     renderMap(map)
@@ -66,13 +77,12 @@ function action(e) {
   } else if (e.keyCode == 37) {
     map = moveLeft(map)
     renderMap(map)
-  } else if ((e.keyCode = 39)) {
+  } else if (e.keyCode == 39) {
     map = moveRight(map)
     renderMap(map)
   } else if (e.keyCode == 32) {
-    console.log('stop')
-    // map = shoot(map)
-    // renderMap(map)
+    let newMap = shoot(map)
+    renderMap(newMap)
   }
   // renderMap(map)
 }
