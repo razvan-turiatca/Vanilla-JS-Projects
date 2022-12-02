36a741c99e40020e269cdffe517a4bf9bdd01a97
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-02 02:04:23 +0200

    added special character

--- Catch-Me-If-You-Can-Game/game.js
+++ Catch-Me-If-You-Can-Game/game.js
@@ -1,6 +1,6 @@
 let map = `
 ##########
-#.p......#
+#..●.....#
 #........#
 #........#
 #........#
@@ -21,36 +21,36 @@ renderMap(map)
 
 // movement
 const moveRight = (map) => {
-  let idx = map.indexOf('p')
+  let idx = map.indexOf('●')
   return map[idx + 1] != '#'
-    ? map.slice(0, idx).concat('.p', map.slice(idx + 2))
+    ? map.slice(0, idx).concat('.●', map.slice(idx + 2))
     : map
 }
 
 const moveLeft = (map) => {
-  let idx = map.indexOf('p')
+  let idx = map.indexOf('●')
   return map[idx - 1] != '#'
-    ? map.slice(0, idx - 1).concat('p.', map.slice(idx + 1))
+    ? map.slice(0, idx - 1).concat('●.', map.slice(idx + 1))
     : map
 }
 
 const moveDown = (map) => {
-  let idx = map.indexOf('p')
+  let idx = map.indexOf('●')
   console.log(map.slice(0, idx))
   return map[idx + 12] != '#'
     ? map
         .slice(0, idx)
         .concat('.', map.slice(idx + 1, idx + 11))
-        .concat('p', map.slice(idx + 12))
+        .concat('●', map.slice(idx + 12))
     : map
 }
 
 const moveUp = (map) => {
-  let idx = map.indexOf('p')
+  let idx = map.indexOf('●')
   return map[idx - 11] != '#'
     ? map
         .slice(0, idx - 11)
-        .concat('p', map.slice(idx - 10, idx))
+        .concat('●', map.slice(idx - 10, idx))
         .concat('.', map.slice(idx + 1))
     : map
 }
