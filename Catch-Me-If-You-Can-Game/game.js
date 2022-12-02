cdec3d975def3f28b224dc5326c439685c77b2de
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-02 18:44:02 +0200

    capture keyboard event and implemented direction movement from the keyboard

--- Catch-Me-If-You-Can-Game/game.js
+++ Catch-Me-If-You-Can-Game/game.js
@@ -1,16 +1,16 @@
 let map = `
-##########
-#..●.....#
-#........#
-#........#
-#........#
-#........#
-#........#
-#........#
-#........#
-#........#
-#........#
-##########
+□□□□□□□□□□
+□▫▫●▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□▫▫▫▫▫▫▫▫□
+□□□□□□□□□□
 `
 
 const renderMap = (map) => {
@@ -22,37 +22,60 @@ renderMap(map)
 // movement
 const moveRight = (map) => {
   let idx = map.indexOf('●')
-  return map[idx + 1] != '#'
-    ? map.slice(0, idx).concat('.●', map.slice(idx + 2))
+  return map[idx + 1] != '□'
+    ? map.slice(0, idx).concat('▫●', map.slice(idx + 2))
     : map
 }
 
 const moveLeft = (map) => {
   let idx = map.indexOf('●')
-  return map[idx - 1] != '#'
-    ? map.slice(0, idx - 1).concat('●.', map.slice(idx + 1))
+  return map[idx - 1] != '□'
+    ? map.slice(0, idx - 1).concat('●▫', map.slice(idx + 1))
     : map
 }
 
 const moveDown = (map) => {
   let idx = map.indexOf('●')
-  console.log(map.slice(0, idx))
-  return map[idx + 12] != '#'
+  // console.log(map.slice(0, idx))
+  return map[idx + 12] != '□'
     ? map
         .slice(0, idx)
-        .concat('.', map.slice(idx + 1, idx + 11))
+        .concat('▫', map.slice(idx + 1, idx + 11))
         .concat('●', map.slice(idx + 12))
     : map
 }
 
 const moveUp = (map) => {
   let idx = map.indexOf('●')
-  return map[idx - 11] != '#'
+  return map[idx - 11] != '□'
     ? map
         .slice(0, idx - 11)
         .concat('●', map.slice(idx - 10, idx))
-        .concat('.', map.slice(idx + 1))
+        .concat('▫', map.slice(idx + 1))
     : map
 }
 
+function action(e) {
+  console.log(e)
+  if (e.code == 'ArrowDown') {
+    map = moveDown(map)
+    renderMap(map)
+  } else if (e.code == 'ArrowUp') {
+    map = moveUp(map)
+    renderMap(map)
+  } else if (e.keyCode == 37) {
+    map = moveLeft(map)
+    renderMap(map)
+  } else if ((e.keyCode = 39)) {
+    map = moveRight(map)
+    renderMap(map)
+  } else if (e.keyCode == 32) {
+    console.log('stop')
+    // map = shoot(map)
+    // renderMap(map)
+  }
+  // renderMap(map)
+}
+
+////////////////////////////////
 renderMap(map)
