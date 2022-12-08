bbf23695e52bef693275e45975efde5a6dece0c3
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-08 13:40:11 +0200

    implemented movement logic for enemy

--- Catch-me-if-you-can-array-game/game.js
+++ Catch-me-if-you-can-array-game/game.js
@@ -7,7 +7,7 @@ let map = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 3, 0, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
@@ -17,10 +17,7 @@ const hero = 2
 const enemy = 3
 const path = 0
 const wall = 1
-const obstacles = [3, 1, '◉']
-const idx = (char) => {
-  return map.indexOf(char)
-}
+const obstacles = [3, 1]
 
 // rendering the game map
 
@@ -42,56 +39,31 @@ const renderMap = (map) => {
   gameMapContainer.innerHTML = output
 }
 
-// movement
+// find coordinates
 
 const obj = {}
 
 const find = (map, el) => {
   map.forEach((arr, i) => {
     if (arr.includes(el)) {
-      obj.row = map.indexOf(arr)
-      obj.col = arr.indexOf(el)
+      obj[`row${el}`] = map.indexOf(arr)
+      obj[`col${el}`] = arr.indexOf(el)
     }
   })
 }
 
+// movement template
+
 const move = (map, char, y, x) => {
-  // find(map, char)
-  if (!obstacles.includes(map[obj.row + y][obj.col + x])) {
-    map[obj.row + y][obj.col + x] = hero
-    map[obj.row][obj.col] = path
+  if (!obstacles.includes(map[obj[`row${char}`] + y][obj[`col${char}`] + x])) {
+    map[obj[`row${char}`] + y][obj[`col${char}`] + x] = char
+    map[obj[`row${char}`]][obj[`col${char}`]] = path
   }
 
   return map
 }
 
-const moveRight = (map, char) => {
-  find(map, char)
-  map[obj.row][obj.col + 1] = char
-  map[obj.row][obj.col] = 0
-  return map
-}
-const moveDown = (map, char) => {
-  find(map, char)
-  map[obj.row + 1][obj.col] = char
-  map[obj.row][obj.col] = 0
-  return map
-}
-
-const moveLeft = (map, char) => {
-  find(map, char)
-
-  map[obj.row][obj.col - 1] = char
-  map[obj.row][obj.col] = 0
-  return map
-}
-
-const moveUp = (map, char) => {
-  find(map, char)
-  map[obj.row - 1][obj.col] = char
-  map[obj.row][obj.col] = 0
-  return map
-}
+// implementing movement logic for hero
 
 function action(e) {
   if (e.code == 'ArrowDown') {
@@ -112,26 +84,46 @@ function action(e) {
   } else if (e.code == 'ArrowRight') {
     find(map, hero)
     move(map, hero, 0, 1)
-    // map[2][3] = hero
 
     renderMap(map)
   }
 }
+
+// implementing movement logic for enemy
 
 function actionEnemy(direction) {
   if (direction == 'Down') {
-    moveDown(map, 3)
+    find(map, enemy)
+    move(map, enemy, 1, 0)
     renderMap(map)
   } else if (direction == 'Up') {
-    moveUp(map, 3)
+    find(map, enemy)
+    move(map, enemy, -1, 0)
     renderMap(map)
   } else if (direction == 'Left') {
-    moveLeft(map, 3)
+    find(map, enemy)
+    move(map, enemy, 0, -1)
     renderMap(map)
   } else if (direction == 'Right') {
-    moveRight(map, 3)
+    find(map, enemy)
+    move(map, enemy, 0, 1)
     renderMap(map)
   }
 }
+
+setInterval(() => {
+  find(map, enemy)
+  find(map, hero)
+  if (obj.row2 < obj.row3) {
+    actionEnemy('Up')
+  } else if (obj.row2 > obj.row3) {
+    actionEnemy('Down')
+  }
+  if (obj.col2 < obj.col3) {
+    actionEnemy('Left')
+  } else if (obj.col2 > obj.col3) {
+    actionEnemy('Right')
+  }
+}, 700)
 
 renderMap(map)
