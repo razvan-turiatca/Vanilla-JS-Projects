bfa59435774ea36e79b1f51139931940d01a539d
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-21 15:33:37 +0200

    imported movie and actor components

--- Movie-App/js/boot.mjs
+++ Movie-App/js/boot.mjs
@@ -1,6 +1,7 @@
 import TMDBService from './services/TMDBService.mjs'
 import StorageService from './services/StorageService.mjs'
-
+import Movie from ' ./domain/Movie.mjs'
+import Actor from './domain/Actor.mjs'
 const tmdbs = new TMDBService()
 const ss = new StorageService()
 
