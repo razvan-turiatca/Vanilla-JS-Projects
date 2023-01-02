5f253d4290af7886173cffd9a4374c541a395162
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-02 23:25:07 +0200

    removed js folder plus files from the ts branch

--- Movie-App/js/boot.mjs
+++
@@ -1,8 +0,0 @@
-import TMDBService from './services/TMDBService.mjs'
-import StorageService from './services/StorageService.mjs'
-import Movie from ' ./domain/Movie.mjs'
-import Actor from './domain/Actor.mjs'
-const tmdbs = new TMDBService()
-const ss = new StorageService()
-
-export { tmdbs, ss }
