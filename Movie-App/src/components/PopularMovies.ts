c16598821e0818d8627921da22ccce7568a6c103
razvan-turiatca <codingwithrazvan@gmail.com>
2023-01-17 23:06:11 +0200

    add carousel functionality

--- Movie-App/src/components/PopularMovies.ts
+++ Movie-App/src/components/PopularMovies.ts
@@ -21,10 +21,31 @@ export default class PopularMovies {
     })
   }
 
-  render(movies: Movie[]) {
-    this.rootElement.innerHTML = ''
-    movies.forEach((movie) => {
-      this.rootElement.innerHTML += `<h3>${movie.title}</h3>`
+  // render(movies: Movie[]) {
+  //   this.rootElement.innerHTML = ''
+  //   movies.forEach((movie) => {
+  //     this.rootElement.innerHTML += `<h3>${movie.title}</h3>`
+  //   })
+  // }
+
+  render = (movies: any[]) => {
+    this.rootElement.innerHTML = `
+        <div class="carousel-item text-center active">
+            <img src="https://image.tmdb.org/t/p/w500/${movies[0].poster}">
+            <h2 class="d-block w-100">${movies[0].title}</h2>
+            <h3>Year: ${movies[0].year}</h3>
+            <span>Rating: ${movies[0].rating}</span>
+        </div>
+    `
+    movies.slice(1).forEach((movie) => {
+      this.rootElement.innerHTML += `
+            <div class="carousel-item text-center">
+                <img src="https://image.tmdb.org/t/p/w500/${movie.poster}">
+                <h2 class="d-block w-100">${movie.title}</h2>
+                <h3>Year: ${movie.year}</h3>
+                <span>Rating: ${movie.rating}</span>
+            </div>
+        `
     })
   }
 }
